df82a166f583d67e4caf80740f818f69c8f65742
beautiful-teacher <124758270+beautiful-teacher@users.noreply.github.com>
2023-07-27 14:08:17 +0800

    Delete assets directory

--- assets/UserInfo-4b7396db.js
+++
@@ -1,1 +0,0 @@
-const s=[{id:"13345678999"},{id:"1814041974@qq.com"}];export{s as u};
--- assets/UserInfo-83169be6.js
+++
@@ -1,1 +0,0 @@
-const q=[{id:"13345678999"},{id:"1814041974@qq.com"},{id:"277832632@qq.com"},{id:"525318834@qq.com"}];export{q as u};
